b24815d5c3758e8252f02f48876b66302c5e1b12
Jacob Smith <jakesmith.seo@gmail.com>
2021-10-17 12:56:40 -0500

    added scroll animation to about section

--- js/app.js
+++ js/app.js
@@ -55,12 +55,17 @@ scrollLinks.forEach((link) => {
 // Add Animation When Scrolled To
 const skill = [...document.querySelectorAll(".skill")];
 const skillIcon = [...document.querySelectorAll(".skill-icon")];
+const aboutImg = document.querySelector(".about-img");
+const aboutPictureContainer = document.querySelector(
+  ".about-picture-container"
+);
 window.addEventListener("scroll", () => {
   let scrollHeight = window.pageYOffset;
   const userTieOffset = skillIcon[0].offsetTop - 200;
   const usersCogOffset = skillIcon[1].offsetTop - 200;
   const networkWiredOffset = skillIcon[2].offsetTop - 200;
   const moneyAltOffset = skillIcon[3].offsetTop - 200;
+  const aboutPictureContainerOffset = aboutPictureContainer.offsetTop - 200;
   if (scrollHeight > userTieOffset) {
     skill[0].classList.add("show-icon");
   } else {
@@ -80,5 +85,12 @@ window.addEventListener("scroll", () => {
     skill[3].classList.add("show-icon");
   } else {
     skill[3].classList.remove("show-icon");
+  }
+  if (scrollHeight > aboutPictureContainerOffset) {
+    aboutImg.classList.add("slide-img");
+    aboutPictureContainer.classList.add("show-slide");
+  } else {
+    aboutImg.classList.remove("slide-img");
+    aboutPictureContainer.classList.remove("show-slide");
   }
 });
