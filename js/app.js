9068af4a8346d6fe605952b950aac9af03bf81f5
Jacob Smith <jakesmith.seo@gmail.com>
2021-10-17 20:48:22 -0500

    fixed scroll animation on step

--- js/app.js
+++ js/app.js
@@ -64,19 +64,24 @@ const serviceImgContainer = [
 ];
 const service = [...document.querySelectorAll(".service")];
 const card = [...document.querySelectorAll(".card")];
+const step = [...document.querySelectorAll(".step")];
 window.addEventListener("scroll", () => {
   let scrollHeight = window.pageYOffset;
   const userTieOffset = skillIcon[0].offsetTop - 200;
   const usersCogOffset = skillIcon[1].offsetTop - 200;
   const networkWiredOffset = skillIcon[2].offsetTop - 200;
   const moneyAltOffset = skillIcon[3].offsetTop - 200;
-  const aboutPictureContainerOffset = aboutPictureContainer.offsetTop - 200;
+  const aboutPictureContainerOffset = aboutPictureContainer.offsetTop - 250;
   const serviceImgContainer1Offset = serviceImgContainer[0].offsetTop - 300;
   const serviceImgContainer2Offset = serviceImgContainer[1].offsetTop - 300;
   const serviceImgContainer3Offset = serviceImgContainer[2].offsetTop - 300;
   const cardOneOffset = card[0].offsetTop - 250;
   const cardTwoOffset = card[1].offsetTop - 250;
   const cardThreeOffset = card[2].offsetTop - 250;
+  const stepOneOffset = step[0].offsetTop - 200;
+  const stepTwoOffset = step[1].offsetTop - 250;
+  const stepThreeOffset = step[2].offsetTop - 250;
+  const stepFourOffset = step[3].offsetTop - 250;
   if (scrollHeight > userTieOffset) {
     skill[0].classList.add("show-icon");
   } else {
@@ -133,5 +138,25 @@ window.addEventListener("scroll", () => {
     card[2].classList.add("card-animation");
   } else {
     card[2].classList.remove("card-animation");
+  }
+  if (scrollHeight > stepOneOffset) {
+    step[0].classList.add("step-transition");
+  } else {
+    step[0].classList.remove("step-transition");
+  }
+  if (scrollHeight > stepTwoOffset) {
+    step[1].classList.add("step-transition");
+  } else {
+    step[1].classList.remove("step-transition");
+  }
+  if (scrollHeight > stepThreeOffset) {
+    step[2].classList.add("step-transition");
+  } else {
+    step[2].classList.remove("step-transition");
+  }
+  if (scrollHeight > stepFourOffset) {
+    step[3].classList.add("step-transition");
+  } else {
+    step[3].classList.remove("step-transition");
   }
 });
